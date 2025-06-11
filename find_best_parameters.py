699afead4eda3f87b8e8f854b507e468e3b0011d
bingolead-stack <talentdev423@gmail.com>
2025-06-11 11:07:03 -0400

    short result available

--- find_best_parameters.py
+++ find_best_parameters.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
 
+OPTIMIZE_LONG = False
 # Load your CSV
-df = pd.read_csv('long_result/optimizer_result.csv') 
+input_file = "long_result/optimizer_result.csv" if OPTIMIZE_LONG else "short_result/optimizer_result.csv"
+output_file = 'long_result/optimizer_final_result.csv' if OPTIMIZE_LONG else 'short_result/optimizer_final_result.csv'
+df = pd.read_csv(input_file) 
 
 def target_function(win_rate, total_pnl, reward_to_risk, num_of_trade,
                     w_win=0, w_pnl=1, w_rr=0, w_trades=0):
@@ -41,7 +44,7 @@ df["SCORE"] = df.apply(
 df_sorted = df.sort_values(by='SCORE', ascending=False)
 
 # Output to a new CSV
-df_sorted.to_csv('long_result/optimizer_final_result.csv', index=False)
+df_sorted.to_csv(output_file, index=False)
 
 # Find the best parameter set
 best_row = df_sorted.loc[df_sorted['SCORE'].idxmax()]
