fea953ce8f7b71f31ed8d61f9bd905631a00707c
bingolead-stack <talentdev423@gmail.com>
2025-08-26 02:59:07 -0400

    short optimizer

--- optimizer.py
+++ optimizer.py
@@ -8,7 +8,7 @@ import pandas as pd
 import os
 
 
-OPTIMIZE_LONG = True
+OPTIMIZE_LONG = False
 
 # Delete previous optimizer result if it exists
 output_file = "strategy/long_result/optimizer_result.csv" if OPTIMIZE_LONG else "strategy/short_result/optimizer_result.csv"
